eb92200429691d8237747e03bba449a8bb2bf331
Mateusz Zubrzycki <122824098+ZuberRS03@users.noreply.github.com>
2023-05-22 23:20:03 +0200

    Add files via upload

---
+++ projekt java101/moto-trivia/src/main/MotoTriviaApplication.java
@@ -0,0 +1,20 @@
+package projekt.java101.mototrivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class MotoTriviaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MotoTriviaApplication.class, args);
+	}
+
+}
